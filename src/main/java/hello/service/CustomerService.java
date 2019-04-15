29b3558e766e0c298f912a46b73110a6eaaf5561
Thomas Johander <tjohander@gmail.com>
2019-04-15 11:18:06 -0400

    Logging added to CustomerService

--- src/main/java/hello/service/CustomerService.java
+++ src/main/java/hello/service/CustomerService.java
@@ -4,6 +4,8 @@ import hello.model.Customer;
 import hello.repository.CustomerRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Service
 public class CustomerService {
@@ -11,9 +13,18 @@ public class CustomerService {
   @Autowired
   private CustomerRespository repository;
 
-  public Customer createCustomer(Customer customer) {
+  final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
-    return repository.save(customer);
+  public Customer createCustomer(Customer customer) {
+    logger.info("Attempting to create customer: " + customer);
+    try {
+      repository.save(customer);
+      logger.info("Customer created: " + customer);
+      return customer;
+    } catch (Exception e) {
+      logger.error("Error saving customer: " + customer + e.getStackTrace());
+      return customer;
+    }
   }
 
 
